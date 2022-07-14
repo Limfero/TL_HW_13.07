0c176d456bcfad9068adf2a102fd741160e9862d
TPAHCJIuT <limon4ik.mail@gmail.com>
2022-07-15 01:08:33 +0300

    Create RawSqlCarDealershipRepository.cs

---
+++ CarProductionApp/Repositories/RawSqlCarDealershipRepository.cs
@@ -0,0 +1,40 @@
+﻿using CarProductionApp.Models;
+using System.Data.SqlClient;
+
+namespace CarProductionApp.Repositories
+{
+    public class RawSqlCarDealershipRepository: ICarDealershipRepository
+    {
+        private readonly string _connectionString;
+
+        public RawSqlCarDealershipRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IReadOnlyList<CarDealership> GetAll()
+        {
+            var result = new List<CarDealership>();
+
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            using SqlCommand sqlCommand = connection.CreateCommand();
+            sqlCommand.CommandText = "select [DealershipId], [NameDealership], [Supplier] from [CarDealership]";
+
+            using SqlDataReader reader = sqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                result.Add(new CarDealership(
+                    Convert.ToInt32(reader["DealershipId"]),
+                    Convert.ToString(reader["NameDealership"]),
+                    Convert.ToInt32(reader["Supplier"])
+                ));
+            }
+
+            return result;
+        }
+
+
+    }
+}
