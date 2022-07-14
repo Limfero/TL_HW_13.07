0c6ec9fc1a8bc30e6b3a16843aa53aafce7d5710
TPAHCJIuT <limon4ik.mail@gmail.com>
2022-07-15 01:08:36 +0300

    Create RawSqlManufacturerRepository.cs

---
+++ CarProductionApp/Repositories/RawSqlManufacturerRepository.cs
@@ -0,0 +1,41 @@
+﻿using CarProductionApp.Models;
+using System.Data.SqlClient;
+
+namespace CarProductionApp.Repositories
+{
+    public class RawSqlManufacturerRepository: IManufacturerRepository
+    {
+        private readonly string _connectionString;
+
+        public RawSqlManufacturerRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IReadOnlyList<Manufacturer> GetAll()
+        {
+            var result = new List<Manufacturer>();
+
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            using SqlCommand sqlCommand = connection.CreateCommand();
+            sqlCommand.CommandText = "select [ManufacturerId], [NameFactory], [Headquarters], [FoundationDate] from [Manufacturer]";
+
+            using SqlDataReader reader = sqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                result.Add(new Manufacturer(
+                    Convert.ToInt32(reader["ManufacturerId"]),
+                    Convert.ToString(reader["NameFactory"]),
+                    Convert.ToString(reader["Headquarters"]),
+                    Convert.ToDateTime(reader["FoundationDate"])
+                ));
+            }
+
+            return result;
+        }
+
+
+    }
+}
