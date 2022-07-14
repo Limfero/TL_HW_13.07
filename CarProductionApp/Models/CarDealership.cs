c6fa6bb58b21c9fef0f7a078824a54efd1cdde41
TPAHCJIuT <limon4ik.mail@gmail.com>
2022-07-15 01:08:07 +0300

    Create CarDealership.cs

---
+++ CarProductionApp/Models/CarDealership.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CarProductionApp.Models
+{
+    public class CarDealership
+    {
+        public int DealershipId { get; private set; }
+        public string NameDealership { get; private set; }
+        public int Supplier { get; private set; }
+
+        public CarDealership(int dealershipId, string nameDealership, int supplier)
+        {
+            DealershipId = dealershipId;
+            NameDealership = nameDealership;
+            Supplier = supplier;
+        }
+    }
+}
