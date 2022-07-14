f7746a26e7642d74506ebd7746116c0ed2bf20d4
TPAHCJIuT <limon4ik.mail@gmail.com>
2022-07-15 01:08:09 +0300

    Create Manufacturer.cs

---
+++ CarProductionApp/Models/Manufacturer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CarProductionApp.Models
+{
+    public class Manufacturer
+    {
+        public int ManufacturerId { get; private set; }
+        public string NameFactory { get; private set; }
+        public string Headquarters { get; private set; }
+        public DateTime FoundationDate { get; private set; }
+
+        public Manufacturer(int manufacturerId, string nameFactory, string headquarters, DateTime foundationDate)
+        {
+            ManufacturerId = manufacturerId;
+            NameFactory = nameFactory;
+            Headquarters = headquarters;
+            FoundationDate = foundationDate;
+        }
+    }
+}
