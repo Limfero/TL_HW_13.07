a1c7d7c585bcdaa584b7a2f58154cb8e8905f1fe
TPAHCJIuT <limon4ik.mail@gmail.com>
2022-07-15 01:08:23 +0300

    Create ICarDealershipRepository.cs

---
+++ CarProductionApp/Repositories/ICarDealershipRepository.cs
@@ -0,0 +1,9 @@
+﻿using CarProductionApp.Models;
+
+namespace CarProductionApp.Repositories
+{
+    public interface ICarDealershipRepository
+    {
+        IReadOnlyList<CarDealership> GetAll();
+    }
+}
