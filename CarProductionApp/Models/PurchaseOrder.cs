360bd64f6a1e22746cb9d3a20cfc852cdbf22a06
TPAHCJIuT <limon4ik.mail@gmail.com>
2022-07-15 01:08:11 +0300

    Create PurchaseOrder.cs

---
+++ CarProductionApp/Models/PurchaseOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CarProductionApp.Models
+{
+    public class PurchaseOrder
+    {
+        public string NameBuyer { get; private set; }
+        public int DealershipId { get; private set; }
+        public int CarId { get; private set; }
+
+        public PurchaseOrder(string nameBuyer, int dealershipId, int carId)
+        {
+            NameBuyer = nameBuyer;
+            DealershipId = dealershipId;
+            CarId = carId;
+        }
+
+
+    }
+}
