edd9f973ea5c98a8e78b01017d1d04bcf6d9534e
TPAHCJIuT <limon4ik.mail@gmail.com>
2022-07-15 01:08:27 +0300

    Create ICarRepository.cs

---
+++ CarProductionApp/Repositories/ICarRepository.cs
@@ -0,0 +1,14 @@
+﻿using CarProductionApp.Models;
+
+namespace CarProductionApp.Repositories
+{
+    public interface ICarRepository
+    {
+        IReadOnlyList<Car> GetAll();
+        IReadOnlyList<Car> GetCarByManufacturerId(int manufacturerId);
+        IReadOnlyList<Car> GroupFromCountOrder(int count);
+        Car GetCarById(int carId);
+        void UpdateCar(Car car);
+        void DeleteCar(Car car);
+    }
+}
